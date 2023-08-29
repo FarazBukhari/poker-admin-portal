b0a29b5b329005cc50b0ce789bbb999bd2137819
Faraz Bukhari <faraz.bukhari@conovoinc.com>
2023-08-30 04:56:06 +0500

    redeploy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Locales from 'ui-component/Locales';
 import NavigationScroll from 'layout/NavigationScroll';
 // import RTLLayout from 'ui-component/RTLLayout';
 import Snackbar from 'ui-component/extended/Snackbar';
-// import Notistack from 'ui-component/third-party/Notistack';
 
 import ThemeCustomization from 'themes';
 
@@ -25,10 +24,8 @@ const App = () => (
             <NavigationScroll>
                 <AuthProvider>
                     <>
-                        {/* <Notistack> */}
                         <Routes />
                         <Snackbar />
-                        {/* </Notistack> */}
                     </>
                 </AuthProvider>
             </NavigationScroll>
